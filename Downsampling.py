10208d4acc9d0a9c854b65d8a9e3bd6c7733cae1
Zhifan Nan <znan@u.rochester.edu>
2021-04-16 17:01:41 -0400

    overwrite

--- Downsampling.py
+++ Downsampling.py
@@ -1,18 +1,16 @@
 import sys
-import pyspark
 from pyspark.sql import SparkSession
 
-def downsampling(spark, hdfs_path, local_path):
+def downsampling(spark, hdfs_path, new_path):
     df = spark.read.parquet(hdfs_path)
-    tiny_sample = df.sample(False, 0.01, seed=0)
-    tiny_sample.write.mode('overwrite').parquet(local_path)
-    print("sample finished")
+    df.write.parquet(new_path)
+    print(df.show(5))
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("downsampling").getOrCreate()
     hdfs_path = sys.argv[1]
-    local_path = sys.argv[2]
-    downsampling(spark, hdfs_path, local_path)
+    new_path = sys.argv[2]
+    downsampling(spark, hdfs_path, new_path)
 
 
 
